36752a59b74be136f6e79891c285a0f679995e5d
Pritesh Jagani <priteshjagani@gmail.com>
2016-01-31 10:56:52 -0800

    First version

--- GameDie.cpp
+++ GameDie.cpp
@@ -1,6 +1,8 @@
+#include "GameDie.h"
 #include <iostream>
 #include <cstdlib>
-
+#include <ctime>
+using namespace std;
 //class constructor that seeds the random number generator
 GameDie::GameDie()
 {
@@ -9,4 +11,9 @@ GameDie::GameDie()
 //generate a random number between 1-6 (inclusive) and display it
 void GameDie::roll()
 {
+  srand(time(NULL)); 
+  
+  cout << "Game Throws number : "
+    << rand() % 6 + 1<<endl;  //rand() helps you generate random number
 }
+
--- GameDie.h
+++ GameDie.h
@@ -8,5 +8,4 @@ class GameDie
    void roll();
 };
 
-#include "GameDie.cpp"
 #endif
